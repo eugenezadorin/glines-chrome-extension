eba3b510297cb6dafd7a470d67d12e86861b0f8e
Eugene Zadorin <mail@eugenezadorin.ru>
2015-09-11 10:07:56 +0300

    working v.0.0.1 alpha

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,44 @@
 window.addEventListener('load', function(){
 	var vLineBtn = document.getElementById('add-vertical-line'),
 		hLineBtn = document.getElementById('add-horizontal-line'),
-		removeAllBtn = document.getElementById('remove-all-lines');
+		removeAllBtn = document.getElementById('remove-all-lines'),
+		lineSizeField = document.getElementById('line-size'),
+		lineColorField = document.getElementById('line-color');
 
+	// get options
+	chrome.storage.sync.get(
+		{
+			// set defaults
+			color: '#000000',
+			size: 4
+		}, 
+		function(items) {
+			lineColorField.value = items.color;
+			lineSizeField.value = items.size;
+		}
+	);
+
+	// save options
+	var saveOptions = function() {
+		chrome.storage.sync.set(
+			{
+				color: lineColorField.value,
+				size: lineSizeField.value
+			}, 
+			function() {
+				var lbl = document.getElementById('settings-lbl');
+				lbl.textContent = 'Settings — saved.';
+				setTimeout(function() {
+					lbl.textContent = 'Settings';
+				}, 750);
+			}
+		);
+	}
+
+	lineSizeField.addEventListener('change', saveOptions);
+	lineColorField.addEventListener('change', saveOptions);
+
+	// bind buttons
 	vLineBtn.addEventListener('click', function(){
 		chrome.tabs.executeScript(
 			null, 
